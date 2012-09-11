ed3ea7ed40ffd49b57866ce064834b3d7a24595e
Michael De Lorenzo <michael@delorenzodesign.com>
2012-09-11 13:29:19 -0400

    Update require paths
    Michael De Lorenzo
    michael@delorenzodesign.com

--- omniauth-bitly.rb
+++ omniauth-bitly.rb
@@ -1,5 +1,2 @@
-require File.join(File.expand_path("../../",__FILE__),"omniauth-bitly","omniauth-bitly","version")
-require File.join(File.expand_path("../../",__FILE__),"omniauth-bitly","omniauth","strategies","bitly")
-
-#require File.join(File.expand(__FILE__"omniauth-bitly/version"
-#require "omniauth/strategies/bitly"
+require "omniauth-bitly/version"
+require "omniauth/strategies/bitly"
